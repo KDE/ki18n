73f35ece14bdf66e04134ab7105e182f30a7ea26
Aurélien Gâteau <agateau@kde.org>
2014-05-05 16:54:05 +0200

    Fix processing of multiple .po files with the same name
    REVIEW: 117997

--- cmake/KF5I18NMacros.cmake
+++ cmake/KF5I18NMacros.cmake
@@ -90,10 +90,83 @@ function(KI18N_INSTALL podir)
         get_filename_component(lang ${lang_dir} NAME)
 
         file(GLOB po_files "${lang_dir}/*.po")
-        gettext_process_po_files(${lang} ALL
+        _ki18n_gettext_process_po_files(${lang} ALL
             INSTALL_DESTINATION ${LOCALE_INSTALL_DIR}
             PO_FILES ${po_files}
         )
         ki18n_install_ts_files(${lang} ${lang_dir}/scripts)
     endforeach()
 endfunction()
+
+
+###############################################################################
+# The following code has been copied from CMake FindGettext.cmake and adjusted
+# to support processing multiple .po files with the same name in different
+# directories.
+#
+# CMake bug report: http://public.kitware.com/Bug/view.php?id=14904
+#
+# This code comes with the following license notice:
+#=============================================================================
+# Copyright 2007-2009 Kitware, Inc.
+# Copyright 2007      Alexander Neundorf <neundorf@kde.org>
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+function(_KI18N_GETTEXT_PROCESS_PO_FILES _lang)
+   set(_options ALL)
+   set(_oneValueArgs INSTALL_DESTINATION)
+   set(_multiValueArgs PO_FILES)
+   set(_gmoFiles)
+
+   CMAKE_PARSE_ARGUMENTS(_parsedArguments "${_options}" "${_oneValueArgs}" "${_multiValueArgs}" ${ARGN})
+
+   foreach(_current_PO_FILE ${_parsedArguments_PO_FILES})
+      get_filename_component(_name ${_current_PO_FILE} NAME)
+      string(REGEX REPLACE "^(.+)(\\.[^.]+)$" "\\1" _basename ${_name})
+      set(_gmoFile ${CMAKE_CURRENT_BINARY_DIR}/${_lang}-${_basename}.gmo)
+      add_custom_command(OUTPUT ${_gmoFile}
+            COMMAND ${GETTEXT_MSGFMT_EXECUTABLE} -o ${_gmoFile} ${_current_PO_FILE}
+            WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+            DEPENDS ${_current_PO_FILE}
+         )
+
+      if(_parsedArguments_INSTALL_DESTINATION)
+         install(FILES ${_gmoFile} DESTINATION ${_parsedArguments_INSTALL_DESTINATION}/${_lang}/LC_MESSAGES/ RENAME ${_basename}.mo)
+      endif()
+      list(APPEND _gmoFiles ${_gmoFile})
+   endforeach()
+
+
+  if(NOT TARGET pofiles)
+     add_custom_target(pofiles)
+  endif()
+
+  _KI18N_GETTEXT_GET_UNIQUE_TARGET_NAME( pofiles uniqueTargetName)
+
+   if(_parsedArguments_ALL)
+      add_custom_target(${uniqueTargetName} ALL DEPENDS ${_gmoFiles})
+   else()
+      add_custom_target(${uniqueTargetName} DEPENDS ${_gmoFiles})
+   endif()
+
+   add_dependencies(pofiles ${uniqueTargetName})
+
+endfunction()
+
+function(_KI18N_GETTEXT_GET_UNIQUE_TARGET_NAME _name _unique_name)
+   set(propertyName "_KI18N_GETTEXT_UNIQUE_COUNTER_${_name}")
+   get_property(currentCounter GLOBAL PROPERTY "${propertyName}")
+   if(NOT currentCounter)
+      set(currentCounter 1)
+   endif()
+   set(${_unique_name} "${_name}_${currentCounter}" PARENT_SCOPE)
+   math(EXPR currentCounter "${currentCounter} + 1")
+   set_property(GLOBAL PROPERTY ${propertyName} ${currentCounter} )
+endfunction()
+# End of CMake copied code ####################################################
