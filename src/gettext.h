10971279a3e77be290da37be4337e8f1c4450565
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-02-13 12:07:53 +0100

    Ki18n: Convert license headers to SPDX statements
    Summary:
    Convert license statements to SPDX and add license text files in LICENSES
    folder as required by the REUSE specification.
    
    Reviewers: cgiboudeaux
    
    Reviewed By: cgiboudeaux
    
    Subscribers: kossebau, cgiboudeaux, kde-frameworks-devel
    
    Tags: #frameworks
    
    Maniphest Tasks: T11550
    
    Differential Revision: https://phabricator.kde.org/D27245

--- src/gettext.h
+++ src/gettext.h
@@ -1,20 +1,8 @@
-/* Convenience header for conditional use of GNU <libintl.h>.
-   Copyright (C) 1995-1998, 2000-2002, 2004-2006 Free Software Foundation, Inc.
+/*  Convenience header for conditional use of GNU <libintl.h>.
+    SPDX-FileCopyrightText: 1995-1998, 2000-2002, 2004-2006 Free Software Foundation, Inc.
 
-   This program is free software; you can redistribute it and/or modify it
-   under the terms of the GNU Library General Public License as published
-   by the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public
-   License along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
-   USA.  */
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
 
 #ifndef _LIBGETTEXT_H
 #define _LIBGETTEXT_H 1
