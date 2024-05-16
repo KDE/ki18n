028465ef2cc8f5758193e545f1ab96ed53a6410d
Emir SARI <emir_sari@icloud.com>
2024-05-16 16:15:34 +0000

    Add helper for spinbox i18n
    This helper enables i18n for localising the Q*SpinBox'es. Normally for QtWidgets applications, it is only possible to add a prefix or a suffix, which does not play well with languages with different word or operator orders.
    
    QtQuick does not have this limitation as far as it seems, but there are a lot of applications that would benefit from this.
    
    Credit goes to Lukas Sommer for this neat implementation.

---
+++ src/i18n/klocalizedspinbox.cpp
@@ -0,0 +1,72 @@
+// SPDX-FileCopyrightText: Lukas Sommer <sommerluk@gmail.com>
+// SPDX-License-Identifier: BSD-2-Clause OR MIT OR Unlicense OR CC0-1.0 OR 0BSD
+
+#ifndef HELPERSPINBOX_H
+#define HELPERSPINBOX_H
+
+#include <qstring.h>
+#include <qstringlist.h>
+#include <type_traits>
+class QDoubleSpinBox;
+class QSpinBox;
+
+/** @internal
+ *
+ * @file
+ *
+ * Provides @ref setSpinboxPrefixSuffix */
+
+/** @internal
+ *
+ * @brief Helper for internationalizing spin boxes.
+ *
+ * Spin boxes display a number and possibly a prefix and/or suffix. However,
+ * in some languages, the position of the prefix and suffix may be reversed
+ * compared to English.
+ *
+ * Example: In English, you write 50%, but in Turkish, you write %50.
+ *
+ * Qt does not offer an out-of-the-box solution for this. This helper now
+ * provides complete internationalization for prefixes and suffixes of
+ * spin boxes.
+ *
+ * @tparam T QSpinBox and QDoubleSpinBox are allowed. This parameter can often
+ *           be omitted.
+ *
+ * @param spinbox Pointer to the spin box.
+ *
+ * @param format A translated string in the format "prefix%1suffix".
+ *
+ * @post If the “format” parameter has the correct format, the spin box's
+ * prefix and suffix will be set accordingly. Otherwise, they will be set
+ * to an empty string.
+ *
+ * Usage example:
+ *
+ * <tt>
+ * setSpinboxPrefixSuffix(mySpinBox, tr("%1%")); // Turkish: "%%1"
+ * </tt>
+ */
+template<typename T>
+void setSpinboxPrefixSuffix(T *spinbox, const QString &format)
+{
+    constexpr auto isDoubleSpin = std::is_base_of<QSpinBox, T>::value;
+    constexpr auto isIntSpin = std::is_base_of<QDoubleSpinBox, T>::value;
+    static_assert(isDoubleSpin || isIntSpin, //
+                  "This template only supports QSpinBox and QDoubleSpinBox.");
+
+    if (spinbox == nullptr) {
+        return;
+    }
+    // QString::arg() support for %L2, %5 etc which translators might expect:
+    const auto l = format.arg(QStringLiteral("%1")).split(QStringLiteral("%1"));
+    if (l.count() == 2) {
+        spinbox->setPrefix(l.at(0));
+        spinbox->setSuffix(l.at(1));
+    } else {
+        spinbox->setPrefix(QString());
+        spinbox->setSuffix(QString());
+    }
+}
+
+#endif // HELPERSPINBOX_H
