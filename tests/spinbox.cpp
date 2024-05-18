08ab2d3998600beac1bca4c5e004fb91ce64613d
Volker Krause <vkrause@kde.org>
2024-05-18 11:49:25 +0200

    Add helper method for setting a plural-aware spin box prefix/suffix
    Essentially a replacement for KPluralHandlingSpinBox from KF::TextWidgets
    that works by composition rather than inheritance, making it easier to use.
    
    Additionally, adding the capability to have translations control whether
    things are placed in the prefix or suffix.
    
    Co-authored with Lukas Sommer <sommerluk@gmail.com>

---
+++ tests/spinbox.cpp
@@ -0,0 +1,26 @@
+/*
+    SPDX-FileCopyrightText: 2024 Volker Krause <vkrause@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include <KLocalization>
+
+#include <QApplication>
+#include <QDoubleSpinBox>
+#include <QSpinBox>
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+
+    QSpinBox spinBox;
+    KLocalization::setupFormatString(&spinBox, ki18np("Baking %v pizza", "Baking %v pizzas"));
+    KLocalization::setupFormatString(&spinBox, ki18np("Baking %v cake", "Baking %v cakes"));
+    spinBox.show();
+
+    QSpinBox doubleSpinBox;
+    KLocalization::setupFormatString(&doubleSpinBox, ki18n("%v%"));
+    doubleSpinBox.show();
+
+    return app.exec();
+}
