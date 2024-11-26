d2c97f39fa16d1112b82073aa995d513d775b359
Nicolas Fella <nicolas.fella@gmx.de>
2024-11-26 21:41:11 +0100

    Add Python bindings
    Co-authored-by: Manuel Alcaraz Zambrano <manuelalcarazzam@gmail.com>

---
+++ python/bindings.h
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2024 Manuel Alcaraz Zambrano <manuelalcarazzam@gmail.com>
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+
+#pragma once
+
+#include <KCountry>
+#include <KCountrySubdivision>
+#include <KLazyLocalizedString>
+#include <KLocalizedContext>
+#include <KLocalizedString>
+#include <KLocalizedTranslator>
+#include <KTimeZone>
+#include <KuitSetup>
