a855492993b193cc55a797573fbbce76e986587a
Friedrich W. H. Kossebau <kossebau@kde.org>
2026-02-16 18:05:10 +0100

    KI18N_WRAP_UI macro: drop now unneeded support for unique image data names
    Support for embedded images, for which the "image([0-9]+)_data" based
    variable names would have been used, got removed from uic for Qt 5.10.
    
    See https://code.qt.io/cgit/qt/qtbase.git/commit/?id=d6d76df

--- cmake/kf6i18nuic.cmake
+++ cmake/kf6i18nuic.cmake
@@ -16,8 +16,6 @@ IF (_uic_CONTENTS)
   #replace tr218n("") with QString() to avoid warning from KLocale
   STRING(REGEX REPLACE "tr2i18n\\(\"\"\\)" "QString\(\)" _uic_CONTENTS "${_uic_CONTENTS}" )
   STRING(REGEX REPLACE "tr2i18n\\(\"\", \"\"\\)" "QString\(\)" _uic_CONTENTS "${_uic_CONTENTS}" )
-  #replace image15_data with img15_filename to make enable_final work
-  STRING(REGEX REPLACE "image([0-9]+)_data" "img\\1_${KDE_UIC_BASENAME}" _uic_CONTENTS "${_uic_CONTENTS}")
   #fixup include guard
   STRING(REGEX REPLACE "#ifndef " "#ifndef UI_" _uic_CONTENTS "${_uic_CONTENTS}")
   STRING(REGEX REPLACE "#define " "#define UI_" _uic_CONTENTS "${_uic_CONTENTS}")
